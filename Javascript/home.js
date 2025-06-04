340f4883a4879723242267e15b7ee0ed1cafe156
pabloperezaw <78221385+pabloperezaw@users.noreply.github.com>
2025-06-04 23:32:40 +0200

    Delete Javascript directory

--- Javascript/home.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const toggle = document.querySelector('.menuDesplegar');
-
-    toggle.querySelector('.submenuTitulo').addEventListener('click', (e) => {
-        e.preventDefault(); // Previene el salto por href="#"
-        toggle.classList.toggle('active');
-    });
-});
-
